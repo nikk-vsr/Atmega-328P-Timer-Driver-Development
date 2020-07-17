120e2a3a7ab59a49cce0a3ab255da4aba8fbf20c
Nikk.Vsr <nikhilvaliveti@gmail.com>
2020-07-17 09:50:43 +0530

    Update Timerconfig.c

--- Timerconfig.c
+++ Timerconfig.c
@@ -1,5 +1,13 @@
-static const Timerconfig_t Timerconfig[]=
-{
-  { 
-    
+#include"Timerconfig.h"
+
+static const Timerconfig_t Timerconfig[]=                               // Timerconfig array with each variable of Timerconfig_t struct data type
+{   // TimerName, TimerEnable , TimerMode, ClkSource, ISREnable, Inerval 
+  {    TMR0,       ENABLED,      UP_COUNT, INTERNAL,   DISABLED, 100},
+  {    TMR1,       DISABLED,     UP_COUNT, INTERNAL,   DISABLED, 100}
+  {    TMR1,       DISABLED,     UP_COUNT, INTERNAL,   DISABLED, 100}
 }
+
+
+
+
+
