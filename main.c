db6e525cae12d72fe86adbbef7a6e97626fa9d68
Nikk.Vsr <nikhilvaliveti@gmail.com>
2020-07-17 09:48:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 
 Tmr_Init()
 
+const Timerconfig_t *TimerConfig = Get_TimerConfig();    // TimerConfig pointer is assigned to Get_TimerConfig function
+
+Tmr_Init(TimerConfig);                // configuration table pointer is passed to the intialization function 
 
 int main()
 {
